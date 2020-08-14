71dc0463c1df529b5600fca7cbe97d76d94ee6c1
fitor27 <bogirawan@gmail.com>
2020-08-15 02:08:19 +0700

    modified:   api/create/create_barang.php

--- api/create/create_barang.php
+++ api/create/create_barang.php
@@ -1,13 +1,13 @@
 <?php
 include_once('../config.php');
 
-$id_barang = addslashes(htmlentities($_POST['id_barang']));
-$nama_barang = addslashes(htmlentities($_POST['nama_barang']));
-$harga_jual = addslashes(htmlentities($_POST['harga_jual']));
-$harga_pokok = addslashes(htmlentities($_POST['harga_pokok']));
-$stok = addslashes(htmlentities($_POST['stok']));
-$update_barang = addslashes(htmlentities($_POST['update_barang']));
-$id_user = addslashes(htmlentities($_POST['id_user']));
+$id_barang = $_POST['id_barang'];
+$nama_barang = $_POST['nama_barang'];
+$harga_jual = $_POST['harga_jual'];
+$harga_pokok = $_POST['harga_pokok'];
+$stok = $_POST['stok'];
+$update_barang = $_POST['update_barang'];
+$id_user = $_POST['id_user'];
 
 $insertnorm = "INSERT INTO barang(id_barang,nama_barang,harga_jual,harga_pokok,stok,update_barang,id_user) 
                VALUES(NULL, '$nama_barang','$harga_jual','$harga_pokok','$stok','$update_barang','$id_user')";
