a85acd8fead675092418e2657a2bd9c6b3a573c9
fitor27 <bogirawan@gmail.com>
2020-08-15 01:21:50 +0700

    modified:   api/create/create_absensi.php

--- api/create/create_absensi.php
+++ api/create/create_absensi.php
@@ -1,23 +1,23 @@
 <?php
-// include_once('../config.php');
+include_once('../config.php');
 
-// $id_absensi = addslashes(htmlentities($_POST['id_absensi']));
-// $tanggal = addslashes(htmlentities($_POST['tanggal']));
-// $login = addslashes(htmlentities($_POST['login']));
-// $id_customer = addslashes(htmlentities($_POST['id_customer']));
+$id_absensi = addslashes(htmlentities($_POST['id_absensi']));
+$tanggal = addslashes(htmlentities($_POST['tanggal']));
+$login = addslashes(htmlentities($_POST['login']));
+$id_customer = addslashes(htmlentities($_POST['id_customer']));
 
-// $insertnorm = "INSERT INTO absensi(id_absensi,tanggal,login,id_customer) 
-//                VALUES(NULL, '$tanggal','$login','$id_customer')";
-// $exeinsertnorm = mysqli_query($koneksi,$insertnorm);
-// $response = array();
-// if($exeinsertnorm)
-// {
-//   $response['code'] =1;
-//   $response['message'] = "Success! Data Inserted";
-// }else{
-//   $response['code'] =0;
-//   $response['message'] = "Failed! Data Not Inserted";
-// }
+$insertnorm = "INSERT INTO absensi(id_absensi,tanggal,login,id_customer) 
+               VALUES(NULL, '$tanggal','$login','$id_customer')";
+$exeinsertnorm = mysqli_query($koneksi,$insertnorm);
+$response = array();
+if($exeinsertnorm)
+{
+  $response['code'] =1;
+  $response['message'] = "Success! Data Inserted";
+}else{
+  $response['code'] =0;
+  $response['message'] = "Failed! Data Not Inserted";
+}
 
 echo json_encode($response);
 
