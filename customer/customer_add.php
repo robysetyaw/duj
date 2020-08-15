afeddb009f3678cb65c167542e9a4d467bc32264
tugaskutugasmu <tugaskutugasmu99@gmail.com>
2020-08-15 20:46:30 +0700

    commit on 'master'

--- customer/customer_add.php
+++ customer/customer_add.php
@@ -1,17 +1,56 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<title>Tambah Customer</title>
-	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>CUSTOMER</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="../css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+
 </head>
-<body>
-<div>
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="customer.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+          <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Daftar Customer<sup></sup></div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+      <li class="nav-item">
+        <a class="nav-link" href="customer_add.php">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Tambah Customer</span></a>
+      </li>
+    </ul> 
+    <div>
 	<form action="../api/customer/create_customer.php" method="post" id="form">
 		<div class="container">
 			
-			<div class="row">
-				<div class="col-sm-5">
-					<h1>Tambah Customer</h1>
+			<div >
+				<div >
+					<h1>Tambah Barang</h1>
 					<p>Masukkan data dengan benar</p>
 					<hr class="mb-3">
 					<label><b>Nama Customer</b></label>
@@ -44,5 +83,4 @@
 		</div>
 	</form>
 </div>
-  </body>
-</html>
+</div>
